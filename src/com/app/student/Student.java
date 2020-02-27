bdc0d272f041cea01ba1df022c73e8fae11e5a5d
rajesh <rajesh@DESKTOP-M4KGAHT>
2020-02-27 09:09:22 +0530

    student commited 1997

---
+++ src/com/app/student/Student.java
@@ -0,0 +1,30 @@
+package com.app.student;
+
+public class Student {
+     private int sid;
+     private String sName;
+     private double fee;
+     
+	public Student() {
+		
+	}
+	public int getSid() {
+		return sid;
+	}
+	public void setSid(int sid) {
+		this.sid = sid;
+	}
+	public String getsName() {
+		return sName;
+	}
+	public void setsName(String sName) {
+		this.sName = sName;
+	}
+	public double getFee() {
+		return fee;
+	}
+	public void setFee(double fee) {
+		this.fee = fee;
+	}
+     
+}
